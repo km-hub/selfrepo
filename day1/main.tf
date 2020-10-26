6d19869a024bdbe59fe874f88d4221a50570d934
ketan Muley <k25muley@gmail.com>
2020-10-26 23:27:27 +0530

    commit 3

--- day1/main.tf
+++ day1/main.tf
@@ -15,7 +15,7 @@ resource "aws_db_instance" "my-test-sql" {
   apply_immediately       = "true"
   backup_retention_period = 10
   backup_window           = "09:46-10:16"
-  db_subnet_group_name    = "${aws_db_subnet_group.my-rds-db-subnet.name}"
+  db_subnet_group_name    = aws_db_subnet_group.my-rds-db-subnet.name
   vpc_security_group_ids  = ["${aws_security_group.my-rds-sg.id}"]
 }
 
