c3e9db7234e3b820c46cc3277f181f7384502a41
ketan Muley <k25muley@gmail.com>
2020-10-26 16:48:09 +0530

    commit 3

--- day1/main.tf
+++ day1/main.tf
@@ -1,7 +1,4 @@
-provider "aws" {
-  region = "eu-west-1"
-}
-
+# DB instance
 resource "aws_db_instance" "default" {
   allocated_storage    = 20
   storage_type         = "gp2"
@@ -12,4 +9,60 @@ resource "aws_db_instance" "default" {
   username             = "master"
   password             = "helloworld"
   parameter_group_name = "default.mysql5.7"
+
+}
+#Subnet Group
+resource "aws_db_subnet_group" "default" {
+  name       = "main"
+  subnet_ids = [aws_subnet.frontend.id, aws_subnet.backend.id]
+
+  tags = {
+    Name = "My DB subnet group"
+  }
+}
+
+#Parameter group
+resource "aws_db_parameter_group" "default" {
+  name   = "rds-pg"
+  family = "mysql5.6"
+
+  parameter {
+    name  = "character_set_server"
+    value = "utf8"
+  }
+
+  parameter {
+    name  = "character_set_client"
+    value = "utf8"
+  }
+}
+
+#Option group
+resource "aws_db_option_group" "example" {
+  name                     = "option-group-test-terraform"
+  option_group_description = "Terraform Option Group"
+  engine_name              = "sqlserver-ee"
+  major_engine_version     = "11.00"
+
+  option {
+    option_name = "Timezone"
+
+    option_settings {
+      name  = "TIME_ZONE"
+      value = "UTC"
+    }
+  }
+
+  option {
+    option_name = "SQLSERVER_BACKUP_RESTORE"
+
+    option_settings {
+      name  = "IAM_ROLE_ARN"
+      value = aws_iam_role.example.arn
+    }
+  }
+
+  option {
+    option_name = "TDE"
+  }
 }
